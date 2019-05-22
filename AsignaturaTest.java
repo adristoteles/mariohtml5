e3a554a39da89b6aab98f8c97255d73962b7283a
iRedsy <johnnyurahara.96@gmail.com>
2019-05-22 10:48:16 +0200

    Add files via upload

---
+++ AsignaturaTest.java
@@ -0,0 +1,14 @@
+package InformacionEstudiante;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AsignaturaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ EstudianteTest.java
@@ -0,0 +1,14 @@
+package InformacionEstudiante;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EstudianteTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
